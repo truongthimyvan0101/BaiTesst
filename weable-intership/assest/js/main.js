2bed450c7ef456a32ab966c2e6fe498f50e0c35b
unknown <you@example.com>
2022-10-04 20:24:54 +0700

    up

--- weable-intership/assest/js/main.js
+++ weable-intership/assest/js/main.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
 
-  $('#toggleOpen').on('click', function() {
-      $('.dtHeader .m-menu .m-navlist').addClass('show');
-      $('.dtHeader .m-menu .overlay').addClass('show');
-      })
-  $('#toggleClose').on('click', function() {
-      $('.dtHeader .m-menu .m-navlist').removeClass('show');
-      $('.dtHeader .m-menu .overlay').removeClass('show');
-  })
-  $('.header .m-menu .overlay').on('click', function() {
-      $('.header .m-menu .m-navlist').removeClass('show');
-      $('.hearder .m-menu .overlay').removeClass('show');
-  })
-});
+    $('#toggleOpen').on('click', function() {
+        $('.header .m-menu .m-navlist').addClass('show');
+        $('.header .m-menu .overlay').addClass('show');
+        })
+    $('#toggleClose').on('click', function() {
+        $('.header .m-menu .m-navlist').removeClass('show');
+        $('.header .m-menu .overlay').removeClass('show');
+    })
+    $('.header .m-menu .overlay').on('click', function() {
+        $('.header .m-menu .m-navlist').removeClass('show');
+        $('.hearder .m-menu .overlay').removeClass('show');
+    })
+  });
