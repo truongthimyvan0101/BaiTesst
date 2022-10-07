fdcfb32863bee5fe33ce18f86c20a431dbef4c48
unknown <you@example.com>
2022-10-07 17:12:13 +0700

    up 07/10/2022

---
+++ weable-intership - 07-10-2022/assest/js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $('#toggleOpen').on('click', function() {
+        $('.header .m-menu .m-navlist').addClass('show');
+        $('.header .m-menu .overlay').addClass('show');
+        })
+    $('#toggleClose').on('click', function() {
+        $('.header .m-menu .m-navlist').removeClass('show');
+        $('.header .m-menu .overlay').removeClass('show');
+    })
+    $('.header .m-menu .overlay').on('click', function() {
+        $('.header .m-menu .m-navlist').removeClass('show');
+        $('.header .m-menu .overlay').removeClass('show');
+    })
+    
+});
+$(document).ready(function(){
+    $('.image-slider').slick({
+        slidesToShow: 3,
+        rows: 2,
+        slidesToScroll: 3,
+        prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+         nextArrow: '<button class="slide-arrow next-arrow"></button>',
+        arrows: true,
+        fade: true,
+        asNavFor: '.slider-nav'
+    });
+});
